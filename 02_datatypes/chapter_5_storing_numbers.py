2007966bb8e444e9277b54a7e781709bca905465
Ratnesh Maurya <ratmaury1@publicisgroupe.net>
2026-05-12 12:32:01 +0530

    added one comment in floating number file

--- 02_datatypes/chapter_5_storing_numbers.py
+++ 02_datatypes/chapter_5_storing_numbers.py
@@ -63,3 +63,8 @@ print(sys.float_info)
 # in java we use - BigDecimals
 # in c++ - we use setPrecision
 # in python we use Decimal("0.1");
+
+
+# Why doesn't 0.1+0.2-0.3 equal 0.0 ?
+# This has to do with floating point accuracy and computer's abilities to represent numbers in memory. 
+# For a full breakdown, check out: https://docs.python.org/2/tutorial/floatingpoint.html
