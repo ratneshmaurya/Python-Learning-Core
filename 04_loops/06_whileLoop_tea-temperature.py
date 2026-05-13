e99113a39d900f67bcae1d49f5926331276f8c00
Ratnesh Maurya <ratmaury1@publicisgroupe.net>
2026-05-13 13:06:42 +0530

    While loop

---
+++ 04_loops/06_whileLoop_tea-temperature.py
@@ -0,0 +1,15 @@
+temperature = 40
+
+while temperature < 100:
+    print(f"Current temperature: {temperature}")
+    # temperature = temperature + 15
+    temperature += 15
+
+print("Tea is ready to boil")
+
+# output:
+# Current temperature: 40
+# Current temperature: 55
+# Current temperature: 70
+# Current temperature: 85
+# Tea is ready to boil
